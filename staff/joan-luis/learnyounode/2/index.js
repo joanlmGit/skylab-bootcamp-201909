a28089aaa4dfb610253e90226514c3a87cde9489
joan <joanlmrr@gmail.com>
2019-11-13 08:37:47 +0100

    add recovered learnyounode and reparate in new branch

---
+++ staff/joan-luis/learnyounode/2/index.js
@@ -0,0 +1,11 @@
+let sumaArray=[];
+let suma;
+
+sumaArray.forEach(element => {
+    suma=suma+element
+});
+	
+	
+}
+//console.log(sumandos+" = " +total);
+console.log(element);
---
+++ staff/joan-luis/learnyounode/5/index.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var path = require('path');
+var mydir = process.argv[2];
+var ext1 = process.argv[3]
+
+
