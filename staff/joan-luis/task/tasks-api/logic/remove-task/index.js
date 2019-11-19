000135ea69a5dc218a6f1e0cecb86f6b179d088a
joan <joanlmrr@gmail.com>
2019-11-19 16:15:42 +0100

    add file remove form manu

--- staff/joan-luis/task/tasks-api/logic/remove-task/index.js
+++ staff/joan-luis/task/tasks-api/logic/remove-task/index.js
@@ -1,3 +1,25 @@
-module.exports=functiion(id,taskId){
-    //todo
+const validate=require('../../utils/validate')
+const database=require('../../utils/database')
+const {NotFoundError, ConflictError}=require('../../utils/errors')
+const {objectId}=database
+
+
+
+module.exports=function(id,taskId, title, description, status){
+    validate.string(id)
+    validate.string.notVoid('id',id)
+
+    if (!objectId.isValid(taskId)) throw new ContentError (`${taskId} is not valid id`)
+
+    if (title){
+        validate.string(title)
+        validate.string.notVoid('title',title)
+    }
+
+    if (description) {
+        validate.string(description)
+        validate.string.notVoid('descriptio',description)
+    }
+
+    if (status)
 }
