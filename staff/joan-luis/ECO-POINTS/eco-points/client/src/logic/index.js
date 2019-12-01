c21f6709be18ba58b29a131320bb6c0c54bd6a20
joan <joanlmrr@gmail.com>
2019-12-01 22:32:59 +0100

    remake files before add client  components logical from manu

---
+++ staff/joan-luis/ECO-POINTS/eco-points/client/src/logic/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    authenticateUser: require('./authenticate-user'),
+    registerUser: require('./register-user'),
+    retrieveUser: require('./retrieve-user'),
+    createPoint: require('./create-point'),
+    listTasks: require('./list-pictures'),
+    modifyPoint: require('./modify-point'),
+}
